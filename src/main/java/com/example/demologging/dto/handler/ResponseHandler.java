54ef83734b612a6bd4ea062e82f9813537e65822
Shiddiq Fizuhri <shiddiq.shadow@gmail.com>
2022-11-24 13:46:33 +0700

    commit 2
    - change HashMap on ResponseHandler.java into LinkedHashMap.
    Hence it'll persist the input order.

--- src/main/java/com/example/demologging/dto/handler/ResponseHandler.java
+++ src/main/java/com/example/demologging/dto/handler/ResponseHandler.java
@@ -3,13 +3,13 @@ package com.example.demologging.dto.handler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ResponseHandler {
 
     public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object datas) {
-        Map<String, Object> response = new HashMap<String, Object>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("message", message);
         response.put("status", status.value());
         response.put("datas", datas);
